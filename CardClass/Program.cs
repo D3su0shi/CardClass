fa7591a32b2c8429efb7a2244cf6db54aa253876
dylan yagninim <dylanmagnym@gmail.com>
2025-09-10 16:51:40 -0400

    lab3a exercise 1

--- CardClass/Program.cs
+++ CardClass/Program.cs
@@ -1,5 +1,9 @@
 ﻿using Cards2;
 
-Card c = new Card(Rank.Ace, Suit.Diamonds);
+Deck d3deck = new Deck();
+d3deck.Shuffle();
+Card firstCard = d3deck.TakeTopCard();
+Console.WriteLine($"The Top card is a {firstCard.Rank} of {firstCard.Suit} \n");
 
-Console.WriteLine(c.Rank + " " + c.Suit);
+Console.WriteLine("Shuffled Deck: ");
+d3deck.Print();
